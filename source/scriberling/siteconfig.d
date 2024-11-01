f20e5f1c5cb58693b459a4c641f95d2789a64a47
Elias Batek <desisma@heidel.beer>
2024-11-01 02:39:49 +0100

    Add site config struct

---
+++ source/scriberling/siteconfig.d
@@ -0,0 +1,34 @@
+module scriberling.siteconfig;
+
+///
+struct SiteConfig {
+	///
+	bool[string] voidElements;
+}
+
+///
+static immutable SiteConfig defaultSiteConfig = () {
+	//
+	auto cfg = SiteConfig();
+	cfg.applyHTML5VoidElements();
+	return cfg;
+}();
+
+@safe:
+
+private void applyHTML5VoidElements(ref SiteConfig cfg) {
+	cfg.voidElements["area"] = true;
+	cfg.voidElements["base"] = true;
+	cfg.voidElements["br"] = true;
+	cfg.voidElements["col"] = true;
+	cfg.voidElements["embed"] = true;
+	cfg.voidElements["hr"] = true;
+	cfg.voidElements["img"] = true;
+	cfg.voidElements["input"] = true;
+	cfg.voidElements["link"] = true;
+	cfg.voidElements["meta"] = true;
+	cfg.voidElements["param"] = true;
+	cfg.voidElements["source"] = true;
+	cfg.voidElements["track"] = true;
+	cfg.voidElements["wbr"] = true;
+}
